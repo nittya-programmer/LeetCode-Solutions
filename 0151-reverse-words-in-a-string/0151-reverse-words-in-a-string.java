d8e7fa6922c8e3ab50bd3ee8ea465f9ae74f588d
Nittya <nittya.jnu@gmail.com>
2024-10-10 20:19:23 +0530

    Time: 24 ms (5.68%), Space: 45.6 MB (6.27%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,27 +1,46 @@
 public class Solution {
     public static String reverseWords(String s) {
-        Stack<String>stack=new Stack<>();
-        String str="";
-        int i=0;
-        while(i<s.length()){
-            if(Character.toString(s.charAt(i)).equals(" ")){
-                if(!str.trim().equals("")){
-                    stack.push(str+" ");
-                }
-                str="";
+        // Stack<String>stack=new Stack<>();//TC:O(n) and SC:O(n)
+        // String str="";
+        // int i=0;
+        // while(i<s.length()){
+        //     if(Character.toString(s.charAt(i)).equals(" ")){
+        //         if(!str.trim().equals("")){
+        //             stack.push(str+" ");
+        //         }
+        //         str="";
+        //     }
+        //     else{
+        //         str+=s.charAt(i);
+        //     }
+        //     i++;
+        // }
+        // stack.push(str+" ");
+        // i=0;
+        // s="";
+        // while(!stack.isEmpty()){
+        //     s+=stack.peek();
+        //     stack.pop();
+        // }
+        // return s.trim();
+        
+        String str1="";
+       String str2="";
+        int i=s.length()-1;
+        while(i>=0){
+            if(!Character.toString(s.charAt(i)).equals(" ")){
+                str1=s.charAt(i)+str1;
             }
             else{
-                str+=s.charAt(i);
+                if(!str1.trim().equals("")){
+                    str2+=str1+s.charAt(i);
+                }
+               str1="";
             }
-            i++;
+            i--;
+            
         }
-        stack.push(str+" ");
-        i=0;
-        s="";
-        while(!stack.isEmpty()){
-            s+=stack.peek();
-            stack.pop();
-        }
-        return s.trim();
+        str2+=str1;
+       return str2.trim(); 
     }
 } 
