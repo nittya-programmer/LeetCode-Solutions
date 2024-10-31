92add7d5e36379a41e23c3344647f4bbc69c69e5
Nittya <nittya.jnu@gmail.com>
2024-10-31 17:22:19 +0530

    Time: 0 ms (100%), Space: 41.3 MB (66.67%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,22 +1,29 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()){
+        // Solution 1
+         if(s.length()!=goal.length()){
             return false;
         }
-        for(int i=0;i<s.length();i++){
-            //Check in each shifted form of string with goal string, if they are idetical 
-            //then return true
-            String msbStr=Character.toString(s.charAt(s.length()-1));
-            String temp="";
-            for(int j=s.length()-1;j>0;j--){
-                temp=s.charAt(j-1)+temp;
-            }
-            temp=msbStr+temp;
-            if(temp.equals(goal)){
-                return true;
-            }
-            s=temp;
-        }
-        return false;
+        s=s+s;
+        return s.contains(goal);
+        //Solution 2
+        // if(s.length()!=goal.length()){
+        //     return false;
+        // }
+        // for(int i=0;i<s.length();i++){
+        //     //Check in each shifted form of string with goal string, if they are idetical 
+        //     //then return true
+        //     String msbStr=Character.toString(s.charAt(s.length()-1));
+        //     String temp="";
+        //     for(int j=s.length()-1;j>0;j--){
+        //         temp=s.charAt(j-1)+temp;
+        //     }
+        //     temp=msbStr+temp;
+        //     if(temp.equals(goal)){
+        //         return true;
+        //     }
+        //     s=temp;
+        // }
+        // return false;
     }
 }
