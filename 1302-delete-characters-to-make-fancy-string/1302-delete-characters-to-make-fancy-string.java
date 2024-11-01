5035d056a88308d686626b8e02ecee5ceff6cdb9
Nittya <nittya.jnu@gmail.com>
2024-11-02 05:17:27 +0530

    Time: 1698 ms (7.84%), Space: 45.6 MB (64.55%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,16 +1,10 @@
 class Solution {
     public String makeFancyString(String s) {
-        // if (s.charAt(0) == s.charAt(1) && s.charAt(1) == s.charAt(2)) {
-        //     s = s.substring(1, s.length());
-        // }
-        // if (s.charAt(s.length() - 3) == s.charAt(s.length() - 2)
-        //         && s.charAt(s.length() - 2) == s.charAt(s.length() - 1)) {
-        //     s = s.substring(s.length() - 1, s.length());
-        // }
+        
         StringBuilder str=new StringBuilder(s);
         for (int i = 1; i < str.length()-1; i++) {
             if (str.charAt(i-1) == str.charAt(i) && str.charAt(i) == str.charAt(i+1)) {
-            // s = s.substring(0, i)+s.substring(i+1,s.length());
+           
             str=str.deleteCharAt(i);
             i--;
         }
@@ -18,3 +12,5 @@ class Solution {
         return str.toString();
     }
 }
+//Time complexity:O(n)
+//Space complexity:O(n)
