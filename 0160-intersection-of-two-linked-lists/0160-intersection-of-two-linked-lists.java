6e7af298d6d41236ff86ad9ed472d1c9eeccbb09
Nittya <nittya.jnu@gmail.com>
2024-11-08 16:33:43 +0530

    Time: 1 ms (99.94%), Space: 48.3 MB (67.59%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -33,48 +33,66 @@ public class Solution {
         //Space Complexity:O(n) //as we used hash map
         //-----------------------------------------------------------
         //-------------------------Optimal Approach------------------
-        if(headA==null||headB==null){
-            return null;
-        }
-        int countA=1;
-        int countB=1;
-        ListNode temp=headA;
-        while(temp.next!=null){
-            temp=temp.next;
-            countA++;
-        }
-        temp=headB;
-        while(temp.next!=null){
-            temp=temp.next;
-            countB++;
-        }
-       ListNode  tempA=headA;
-        ListNode tempB=headB;
-        int i=1;
-        if(countA>countB){
-            while(i<=countA-countB){
-                tempA=tempA.next;
-                i++;
-            }
-        }
-        else{
-             while(i<=countB-countA){
-                tempB=tempB.next;
-                i++;
-            }
-        }
-        while(tempA!=null && tempA.next!=null){
-            if(tempA==tempB){
-                return tempA;
-            }
-            tempA=tempA.next;
-            tempB=tempB.next;
-        }
-        if(tempA==tempB){//edge case , for last node or when ll have single node
+    //     if(headA==null||headB==null){
+    //         return null;
+    //     }
+    //     int countA=1;
+    //     int countB=1;
+    //     ListNode temp=headA;
+    //     while(temp.next!=null){
+    //         temp=temp.next;
+    //         countA++;
+    //     }
+    //     temp=headB;
+    //     while(temp.next!=null){
+    //         temp=temp.next;
+    //         countB++;
+    //     }
+    //    ListNode  tempA=headA;
+    //     ListNode tempB=headB;
+    //     int i=1;
+    //     if(countA>countB){
+    //         while(i<=countA-countB){
+    //             tempA=tempA.next;
+    //             i++;
+    //         }
+    //     }
+    //     else{
+    //          while(i<=countB-countA){
+    //             tempB=tempB.next;
+    //             i++;
+    //         }
+    //     }
+    //     while(tempA!=null && tempA.next!=null){
+    //         if(tempA==tempB){
+    //             return tempA;
+    //         }
+    //         tempA=tempA.next;
+    //         tempB=tempB.next;
+    //     }
+    //     if(tempA==tempB){//edge case , for last node or when ll have single node
+    //         return tempA;
+    //     }
+
+    //     return null;
+    //Another optimal approach
+    ListNode tempA=headA;
+    ListNode tempB=headB;
+    while(tempA!=tempB){
+        tempA=tempA.next;
+        tempB=tempB.next;
+        if(tempA==tempB){// if ll have no collision then it will return null
             return tempA;
         }
-
-        return null;
+        if(tempA==null){
+            tempA=headB;
+        }
+        if(tempB==null){
+            tempB=headA;
+        }
+        
+    }
+    return tempA;//if first node is collision node 
 
 
 
