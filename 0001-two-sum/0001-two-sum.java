e5b730603dae07ac819344204a90034a7e01234c
Nittya <nittya.jnu@gmail.com>
2025-05-10 03:52:55 +0530

    Time: 2 ms (98.82%), Space: 45 MB (63.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //brute force: compare each and every pair
+        //brute force: compare each and every pair 
+        //time complexity:O(n2)
         /*
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -12,6 +13,8 @@ class Solution {
         return new int[]{-1,-1};
         */
         //using hashmap: for both when given array is sorted and unsorted
+        //time complexity:O(n)
+        
         HashMap<Integer,Integer>hmap=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             int diff=target-nums[i];
@@ -21,6 +24,22 @@ class Solution {
             hmap.put(nums[i],i);
         }
         return new int[]{-1,-1};
+        
         //using two pointer: only when given array is sorted
+        //time complexity:O(n)
+        /*int leftPointer=0,rightPointer=nums.length-1;
+        while(leftPointer<=rightPointer){
+            if(nums[leftPointer]+nums[rightPointer]>target){
+                rightPointer--;
+            }
+            else if(nums[leftPointer]+nums[rightPointer]<target){
+                leftPointer++;
+            }
+            else{
+                return new int[]{leftPointer,rightPointer};
+            }
+        }
+        return new int[]{-1,-1};
+        */
     }
 }
