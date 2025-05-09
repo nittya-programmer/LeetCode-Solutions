2ac36bfb335058004d554760f6f146951f4dc10d
Nittya <nittya.jnu@gmail.com>
2025-05-10 03:47:20 +0530

    Time: 2 ms (98.82%), Space: 45.3 MB (9.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         //brute force: compare each and every pair
+        /*
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -9,7 +10,17 @@ class Solution {
             }
         }
         return new int[]{-1,-1};
+        */
         //using hashmap: for both when given array is sorted and unsorted
+        HashMap<Integer,Integer>hmap=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int diff=target-nums[i];
+            if(hmap.containsKey(diff)){
+                return new int[]{hmap.get(diff),i};
+            }
+            hmap.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
         //using two pointer: only when given array is sorted
     }
 }
