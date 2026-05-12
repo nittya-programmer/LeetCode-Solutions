5dacef7fad17e106910db84c7623c5d3639fc38a
Nittya <nittya.jnu@gmail.com>
2026-05-13 05:08:27 +0530

    Time: 44 ms (31.79%), Space: 47.2 MB (23.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,45 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //brute force: compare each and every pair 
-        //time complexity:O(n2)
-        /*
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
+        int start=0, end=nums.length-1;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
                 }
             }
         }
-        return new int[]{-1,-1};
-        */
-        //using hashmap: for both when given array is sorted and unsorted
-        //time complexity:O(n)
-        
-        HashMap<Integer,Integer>hmap=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int diff=target-nums[i];
-            if(hmap.containsKey(diff)){
-                return new int[]{hmap.get(diff),i};
-            }
-            hmap.put(nums[i],i);
-        }
-        return new int[]{-1,-1};
-        
-        //using two pointer: only when given array is sorted
-        //time complexity:O(n)
-        /*int leftPointer=0,rightPointer=nums.length-1;
-        while(leftPointer<=rightPointer){
-            if(nums[leftPointer]+nums[rightPointer]>target){
-                rightPointer--;
-            }
-            else if(nums[leftPointer]+nums[rightPointer]<target){
-                leftPointer++;
-            }
-            else{
-                return new int[]{leftPointer,rightPointer};
-            }
-        }
-        return new int[]{-1,-1};
-        */
+        return new int[]{0,0};
+
     }
 }
